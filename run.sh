7773c68cac67a66134b47f91f26a5206d7aa958e
James Robb <james@jamesrobb.co.uk>
2019-03-29 16:38:55 +0100

    tidying up

--- run.sh
+++ run.sh
@@ -3,16 +3,10 @@ DEBUG_LOG_PRETTY=true accessible-pipeline run $1 --ci --pageLimit 1 --ignoreFrag
 reportFile=$(ls | grep "^report-")
 stateFile=$(ls | grep "^state-")
 
-echo $reportFile
-echo $stateFile
-
 cd parser
 npm run start -- --inputFile "../$reportFile" --outputFile report.json
-mv report.json ../report.json
+mv report.json ../$2.json
 cd ..
 
 rm $reportFile
 rm $stateFile
-
-cd server
-npm run start
